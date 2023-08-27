1b2631f8a028433e9cb9556a50c3c8c730332bb5
Livni <llmc0383@gmail.com>
2023-08-27 00:36:48 -0600

    se agrego el modificar y eliminar

--- controllers/RolController.php
+++ controllers/RolController.php
@@ -37,7 +37,7 @@ class RolController {
     {
         $rol_nombre = $_GET['rol_nombre'] ?? '';
 
-        $sql = "SELECT * FROM roles WHERE rol_situacion = 1 ";
+        $sql = "SELECT * FROM roles WHERE rol_situacion = '1' ";
         if ($rol_nombre != '') {
             $rol_nombre = strtolower($rol_nombre);
             $sql .= " AND LOWER(rol_nombre) LIKE '%$rol_nombre%' ";
@@ -56,4 +56,60 @@ class RolController {
             ]);
         }
     }
+
+
+public static function modificarAPI(){
+    try {
+        $rol = new Rol($_POST);
+        $resultado = $rol->actualizar();
+
+        if ($resultado['resultado'] == 1) {
+            echo json_encode([
+                'mensaje' => 'Registro modificado correctamente',
+                'codigo' => 1
+            ]);
+        } else {
+            echo json_encode([
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+        // echo json_encode($resultado);
+    } catch (Exception $e) {
+        echo json_encode([
+            'detalle' => $e->getMessage(),
+            'mensaje' => 'Ocurrió un error',
+            'codigo' => 0
+        ]);
+    }
+}
+
+
+public static function eliminarAPI(){
+    try {
+        $rol_id = $_POST['rol_id'];
+        $rol = Rol::find($rol_id);
+        $rol->rol_situacion = 0;
+        $resultado = $rol->actualizar();
+
+        if ($resultado['resultado'] == 1) {
+            echo json_encode([
+                'mensaje' => 'Registro eliminado correctamente',
+                'codigo' => 1
+            ]);
+        } else {
+            echo json_encode([
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+        // echo json_encode($resultado);
+    } catch (Exception $e) {
+        echo json_encode([
+            'detalle' => $e->getMessage(),
+            'mensaje' => 'Ocurrió un error',
+            'codigo' => 0
+        ]);
+    }
+  }
 }
