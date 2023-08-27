c3323c12bc0b2bff6a78fd1bc4eef9969ba05c34
Livni <llmc0383@gmail.com>
2023-08-27 01:08:05 -0600

    se agrego modificar y eliminar

--- src/js/usuarios/index.js
+++ src/js/usuarios/index.js
@@ -2,14 +2,14 @@ import { Dropdown } from "bootstrap";
 import Swal from "sweetalert2";
 import { validarFormulario, Toast, confirmacion} from "../funciones";
 import { lenguaje  } from "../lenguaje";
+import Datatable from "datatables.net-bs5";
 
-
-const formulario = document.querySelector('form')
+const formulario = document.querySelector('formularioUsuario')
 const btnGuardar = document.getElementById('btnGuardar');
 const btnBuscar = document.getElementById('btnBuscar');
 const btnModificar = document.getElementById('btnModificar');
 const btnCancelar = document.getElementById('btnCancelar');
-
+const divTabla = document.getElementById('#tablaUsuarios');
 
 btnModificar.disabled = true
 btnModificar.parentElement.style.display = 'none'
@@ -152,13 +152,67 @@ const guardar = async (evento) => {
     }
 }
 
+const eliminar = async (e) => {
+    const button = e.target;
+    const id = button.dataset.id;
+
+    if (await confirmacion('warning', '¿Desea eliminar este registro?')) {
+        const body = new FormData();
+        body.append('usu_id', id);
+        const url = '/parcial_martinez/API/usuarios/eliminar';
+        const config = {
+            method: 'POST',
+            body
+        };
+
+        try {
+            const respuesta = await fetch(url, config);
+            const data = await respuesta.json();
+
+            const { codigo, mensaje, detalle } = data;
+            let icon = 'info';
+            switch (codigo) {
+                case 1:
+                    icon = 'success';
+                    buscar();
+                    break;
+
+                case 0:
+                    icon = 'error';
+                    console.log(detalle);
+                    break;
+
+                default:
+                    break;
+            }
+            Toast.fire({
+                icon,
+                text: mensaje
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+const cancelarAccion = () => {
+    btnGuardar.disabled = false
+    btnGuardar.parentElement.style.display = ''
+    btnBuscar.disabled = false
+    btnBuscar.parentElement.style.display = ''
+    btnModificar.disabled = true
+    btnModificar.parentElement.style.display = 'none'
+    btnCancelar.disabled = true
+    btnCancelar.parentElement.style.display = 'none'
+    divTabla.style.display = ''
+}
+
 const traeDatos = (e) => {
     const button = e.target;
     const id = button.dataset.id;
     const nombre = button.dataset.nombre;
     const catalogo = button.dataset.catalogo;
     const password = button.dataset.password;
-    
 
     const dataset = {
         id,
@@ -166,6 +220,7 @@ const traeDatos = (e) => {
         catalogo,
         password
     };
+
     colocarDatos(dataset);
         const body = new FormData(formulario);
         body.append('usu_id', id);
@@ -175,6 +230,86 @@ const traeDatos = (e) => {
 
 };
 
+const colocarDatos = (dataset) => {
+    formulario.usu_nombre.value = dataset.nombre;
+    formulario.usu_catalogo.value = dataset.catalogo;
+    formulario.usu_password.value = dataset.password;
+    formulario.usu_id.value = dataset.id;
+    
+    btnGuardar.disabled = true;
+    btnGuardar.parentElement.style.display = 'none';
+    btnBuscar.disabled = true;
+    btnBuscar.parentElement.style.display = 'none';
+    
+    btnModificar.disabled = false;
+    btnModificar.parentElement.style.display = '';
+    btnCancelar.disabled = false;
+    btnCancelar.parentElement.style.display = '';
+    
+    divTabla.style.display = 'none';
+};
+
+
+const modificar = async () => {
+  
+    if (!validarFormulario(formulario)) {
+        Toast.fire({
+            icon: 'info',
+            text: 'Debe llenar todos los datos'
+        })
+        return
+    }
+
+    const body = new FormData(formulario)
+    const url = '/parcial_martinez/API/usuarios/modificar';
+    const headers = new Headers();
+    headers.append("X-Requested-With","fetch");
+    const config = {
+        method: 'POST',
+        body
+    }
+
+    try {
+        // fetch(url, config).then( (respuesta) => respuesta.json() ).then(d => data = d)
+        const respuesta = await fetch(url, config)
+        const data = await respuesta.json();
+        //    console.log(data);
+        //    return;
+
+        const { codigo, mensaje, detalle } = data;
+        let icon = 'info'
+        switch (codigo) {
+            case 1:
+                formulario.reset();
+                Toast.fire({
+                    icon: 'success',
+                    text: mensaje
+                })
+                buscar();
+                cancelarAccion();
+                break;
+
+            case 0:
+                Toast.fire({
+                    icon: 'error',
+                    text: mensaje
+                })
+                console.log(detalle)
+                break;
+
+            default:
+                break;
+        }
+
+        Toast.fire({
+            icon,
+            text: mensaje
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
 buscar();
