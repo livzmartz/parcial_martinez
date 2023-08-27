a51c9619c11d2542a51b365d681612a81a777f12
Livni <llmc0383@gmail.com>
2023-08-26 22:09:24 -0600

    se agregaron las rutas de los controladores al index

--- public/index.php
+++ public/index.php
@@ -4,10 +4,28 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\AppController;
+use Controllers\LoginController;
+use Controllers\UsuarioController;
+use Controllers\RolController;
+use Controllers\PermisoController;
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
-$router->get('/', [AppController::class,'index']);
+$router->get('/', [LoginController::class,'index']);
+$router->get('/usuarios', [LoginController::class,'usuarios']);
+$router->get('/logout', [LoginController::class,'logout']);
+$router->post('/API/login', [LoginController::class,'loginAPI']);
+
+
+$router->get('/API/usuarios/buscar', [UsuarioController::class,'buscarAPI']);
+$router->post('/API/usuarios/guardar', [UsuarioController::class,'guardarAPI'] );
+
+$router->get('/API/roles/buscar', [RolController::class,'buscarAPI'] );
+$router->post('/API/roles/guardar', [RolController::class,'guardarAPI'] );
+
+$router->get('/API/permisos/buscar', [PermisoController::class,'buscarAPI'] );
+$router->post('/API/permisos/guardar', [PermisoController::class,'guardarAPI'] );
+
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
